67421b56d2cb6bb1d55c3615a1d51b414208c800
Chris Bystrek <chris.bystrek@gmail.com>
2019-12-08 07:14:24 -0500

    Camera Testing.

--- src/scrolly.js
+++ src/scrolly.js
@@ -1,8 +1,5 @@
 import React, { Component } from 'react'
 import './styles.css'
-import AppBar from '@material-ui/core/AppBar'
-import NavBar from './NavBar'
-import { Paper } from '@material-ui/core';
 
 export default class Scrolly extends Component {
     constructor(props) {
@@ -14,18 +11,21 @@ export default class Scrolly extends Component {
     render() {
         console.log(this.props.feed)
         return (
-            <div style = {{backgroundColor:'grey'}}>
-                {/* <NavBar/> */}
-                <div>
-                    {this.props.feed.map((i, index) => (
-                        // <Paper style = {{margin:20,}}>
-                        <div style={{borderBottom: "5px solid black"}}> 
-                            <img src= {i.url} className = "item" key={index}/>
-                        </div>
-                        // </Paper>
-                    ))}
-                </div>
-            </div>
+            // <div style = {{backgroundColor:'grey'}}>
+            // <div style={{position: "fixed", bottom: "0", backgroundColor: "#1976d2", right: "40%",height: "150px", width: "150px", marginBottom: "80px", borderRadius: "50%", boxShadow: "0px 3px 5px rgba(0,0,0,0.3"}}>
+            // </div>
+            //     {/* <NavBar/> */}
+            //     <div>
+            //         {this.props.feed.map((i, index) => (
+            //             // <Paper style = {{margin:20,}}>
+            //             <div style={{borderBottom: "5px solid black"}}> 
+            //                 <img src= {i.url} className = "item" key={index}/>
+            //             </div>
+            //             // </Paper>
+            //         ))}
+            //     </div>
+            // </div>
+            <input type="file" accept="image/*;capture=camera"></input>
         );
     }
 }
